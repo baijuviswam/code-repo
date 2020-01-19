91f62f9e38400f0da07920609f82398d60a032d2
Baijumon Viswambharan <bkviswam@Baijumons-MacBook-Pro.local>
2020-01-18 16:48:53 -0800

    Wrong comment changed

--- com/lab/ds/dcp/ReverseInteger.java
+++ com/lab/ds/dcp/ReverseInteger.java
@@ -1,16 +1,5 @@
 package com.lab.ds.dcp;
 
-/**
-
-Daily Coding Problem: Problem #202 [Easy]
-
-
-This problem was asked by Palantir.
-
-Write a program that checks whether an integer is a palindrome. For example, 121 is a palindrome, as well as 888. 678 is not a palindrome. Do not convert the integer into a string.
-
-**/
-
 public class ReverseInteger{
 	public static void main(String[] args) {
 		ReverseInteger ri = new ReverseInteger();
