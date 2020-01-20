57df35de9ab58c34b6c65645c50f85e2488ff151
bkviswam <baiju.k.viswam@gmail.com>
2020-01-19 17:50:29 -0800

    Update ReverseInteger.java

--- com/lab/ds/dcp/ReverseInteger.java
+++ com/lab/ds/dcp/ReverseInteger.java
@@ -1,18 +1,29 @@
 package com.lab.ds.dcp;
 
-public class ReverseInteger{
+/**
+
+This problem was asked by Palantir.
+
+Write a program that checks whether an integer is a palindrome. For example, 121 is a palindrome, as well as 888. 678 is not a palindrome. 
+Do not convert the integer into a string.
+
+**/
+
+public class IntegerPalindrome{
 	public static void main(String[] args) {
-		ReverseInteger ri = new ReverseInteger();
+		IntegerPalindrome ri = new IntegerPalindrome();
 		int num = Integer.MIN_VALUE;
-		System.out.println("Number :"+num);
-		System.out.println("Reversed :"+ri.reverseInt(num));
+		//System.out.println("Number :"+num);
+		System.out.println("Is Palindrome :"+ri.isPalindrom(num));
 	}
-	public long reverseInt(int num){
+	public boolean isPalindrom(int num){
+		long cpy = num;
 		long reversedInt = 0;
 		while(num!=0){
-			reversedInt = reversedInt *10 + num%10;	
+			reversedInt = reversedInt *10 + num % 10;	
 			num=num/10;
 		}
-		return reversedInt;
+		if(reversedInt == cpy) return true;
+		return false;
 	}
 }
